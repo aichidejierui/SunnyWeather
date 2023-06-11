dbe203114d3926fc331b2bcb250459710b91df91
jemo <jemo@gmail.com>
2023-06-11 22:15:09 +0800

    First commit.

---
+++ app/src/main/java/com/example/sunnyweather/ui/place/PlaceFragment.kt
@@ -0,0 +1,69 @@
+package com.example.sunnyweather.ui.place
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.core.widget.addTextChangedListener
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.sunnyweather.databinding.FragmentPlaceBinding
+
+class PlaceFragment : Fragment() {
+
+    private var _binding: FragmentPlaceBinding? = null
+
+    private val fragmentPlaceBinding: FragmentPlaceBinding get() = _binding!!
+
+    val viewModel by lazy { ViewModelProvider(this).get(PlaceViewModel::class.java) }
+
+    private lateinit var adapter: PlaceAdapter
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        _binding = FragmentPlaceBinding.inflate(inflater, container, false)
+
+        return fragmentPlaceBinding.root
+    }
+
+    @Deprecated("Deprecated in Java")
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        val layoutManager = LinearLayoutManager(activity)
+        fragmentPlaceBinding.recyclerView.layoutManager = layoutManager
+        adapter = PlaceAdapter(this, viewModel.placeList)
+        fragmentPlaceBinding.recyclerView.adapter = adapter
+        fragmentPlaceBinding.searchPlaceEdit.addTextChangedListener { editable ->
+            val content = editable.toString()
+            if (content.isNotEmpty()) {
+                viewModel.searchPlaces(content)
+            } else {
+                fragmentPlaceBinding.recyclerView.visibility = View.GONE
+                fragmentPlaceBinding.bgImageView.visibility = View.VISIBLE
+                viewModel.placeList.clear()
+                adapter.notifyDataSetChanged()
+            }
+        }
+
+        viewModel.placeLiveData.observe(viewLifecycleOwner, Observer{ result ->
+            val places = result.getOrNull()
+            if (places != null) {
+                fragmentPlaceBinding.recyclerView.visibility = View.VISIBLE
+                fragmentPlaceBinding.bgImageView.visibility = View.GONE
+                viewModel.placeList.clear()
+                viewModel.placeList.addAll(places)
+                adapter.notifyDataSetChanged()
+            } else {
+                Toast.makeText(activity, "未能查询到任何地点", Toast.LENGTH_SHORT).show()
+                result.exceptionOrNull()?.printStackTrace()
+            }
+        })
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
