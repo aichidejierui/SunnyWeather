b2d8fdfe86475daa3090916d51626dcdd715910a
jemo <jemo@gmail.com>
2023-06-12 20:07:16 +0800

    加入显示天气信息的功能

--- app/src/main/java/com/example/sunnyweather/logic/network/ServiceCreator.kt
+++ app/src/main/java/com/example/sunnyweather/logic/network/ServiceCreator.kt
@@ -10,6 +10,7 @@ object ServiceCreator {
         .baseUrl(BASE_URL)
         .addConverterFactory(GsonConverterFactory.create())
         .build()
+
     fun <T> create(serviceClass: Class<T>): T = retrofit.create(serviceClass)
     
     inline fun <reified T> create(): T = create(T::class.java)
---
+++ app/src/main/java/com/example/sunnyweather/ui/weather/WeatherActivity.kt
@@ -0,0 +1,111 @@
+package com.example.sunnyweather.ui.weather
+
+import android.graphics.Color
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import com.example.sunnyweather.R
+import com.example.sunnyweather.databinding.ActivityWeatherBinding
+import com.example.sunnyweather.databinding.ForecastBinding
+import com.example.sunnyweather.databinding.LifeIndexBinding
+import com.example.sunnyweather.databinding.NowBinding
+import com.example.sunnyweather.logic.model.Weather
+import com.example.sunnyweather.logic.model.getSky
+import java.text.SimpleDateFormat
+import java.util.Locale
+
+class WeatherActivity : AppCompatActivity() {
+
+    val viewModel by lazy { ViewModelProvider(this).get(WeatherViewModel::class.java) }
+
+    lateinit var now : NowBinding
+    lateinit var forecast : ForecastBinding
+    lateinit var lifeIndexBinding: LifeIndexBinding
+    lateinit var activityWeather: ActivityWeatherBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_weather)
+
+        if (viewModel.locationLng.isEmpty()) {
+            viewModel.locationLng = intent.getStringExtra("location_lng") ?: ""
+        }
+
+        if (viewModel.locationLat.isEmpty()) {
+            viewModel.locationLat = intent.getStringExtra("location_lat") ?: ""
+        }
+
+        if (viewModel.placeName.isEmpty()) {
+            viewModel.placeName = intent.getStringExtra("place_name") ?: ""
+        }
+
+        viewModel.weatherLiveData.observe(this, Observer { result ->
+            val weather = result.getOrNull()
+            if (weather != null) {
+                showWeatherInfo(weather)
+            } else {
+                Toast.makeText(this, "无法成功获取天气信息", Toast.LENGTH_SHORT).show()
+                result.exceptionOrNull()?.printStackTrace()
+            }
+        })
+        viewModel.refreshWeather(viewModel.locationLng, viewModel.locationLat)
+    }
+
+    private fun showWeatherInfo(weather: Weather) {
+        now = NowBinding.inflate(layoutInflater)
+        forecast = ForecastBinding.inflate(layoutInflater)
+        lifeIndexBinding = LifeIndexBinding.inflate(layoutInflater)
+        activityWeather = ActivityWeatherBinding.inflate(layoutInflater)
+        setContentView(now.root)
+        setContentView(forecast.root)
+        setContentView(lifeIndexBinding.root)
+        setContentView(activityWeather.root)
+
+        now.placeName.text = viewModel.placeName
+        val realtime = weather.realtime
+        val daily = weather.daily
+
+        // 填充now.xml布局中的数据
+        val currentTempText = "${realtime.temperature.toInt()} ℃"
+        now.currentTemp.text = currentTempText
+        now.currentSky.text = getSky(realtime.skycon).info
+        val currentPM25Text = "空气指数 ${realtime.airQuality.aqi.chn.toInt()}"
+        now.currentAQI.text = currentPM25Text
+        now.nowLayout.setBackgroundResource(getSky(realtime.skycon).bg)
+
+        // 填充forecast.xml布局中的数据
+        forecast.forecastLayout.removeAllViews()
+        val days = daily.skycon.size
+        for (i in 0 until days) {
+            val skycon = daily.skycon[i]
+            val temperature = daily.temperature[i]
+            val view = LayoutInflater.from(this).inflate(R.layout.forecast_item, forecast.forecastLayout, false)
+            val dateInfo = view.findViewById(R.id.dateInfo) as TextView
+            val skyIcon = view.findViewById(R.id.skyIcon) as ImageView
+            val skyInfo = view.findViewById(R.id.skyInfo) as TextView
+            val temperatureInfo = view.findViewById(R.id.temperatureInfo) as TextView
+            val simpleDateFormat = SimpleDateFormat("yyyy-MM-dd", Locale.getDefault())
+            dateInfo.text = simpleDateFormat.format(skycon.date)
+            val sky = getSky(skycon.value)
+            skyIcon.setImageResource(sky.icon)
+            skyInfo.text = sky.info
+            val tempText = "${temperature.min.toInt()} ~ ${temperature.max.toInt()} ℃"
+            temperatureInfo.text = tempText
+            forecast.forecastLayout.addView(view)
+        }
+
+        // 填充life_index.xml布局中的数据
+        val lifeIndex = daily.lifeIndex
+        lifeIndexBinding.coldRiskText.text = lifeIndex.coldRisk[0].desc
+        lifeIndexBinding.dressingText.text = lifeIndex.dressing[0].desc
+        lifeIndexBinding.ultravioletText.text = lifeIndex.ultraviolet[0].desc
+        lifeIndexBinding.carWashingText.text = lifeIndex.carWashing[0].desc
+        activityWeather.weatherLayout.visibility = View.VISIBLE
+    }
+}
